d6efb7f2ab38bca8f10d90df049ce4ec34a141d8
dwivedialind <dwivedialind@gmail.com>
2025-05-11 18:25:48 +0530

    added a about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
    root "pages#home"
+
+   get 'about', to: 'pages#about'
 end
